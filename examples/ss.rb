71e87c839cd9a779c509d4d75fb4677748628b5a
yang <yangkang@liveneeq.com>
2016-09-21 18:56:24 +0800

    Add daemon ss

--- examples/ss.rb
+++ examples/ss.rb
@@ -1,9 +1,75 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yyrp'
+require 'logging'
 
-EventMachine::run {
-  server = Yyrp::ShadowsocksServer.new
-  Signal.trap('INT') { server.stop }
-  Signal.trap('TERM') { server.stop }
-  server.start
-}
+@pid_full = '/tmp/shadowsocks.pid'
+
+def run
+  EventMachine::run {
+    server = Yyrp::ShadowsocksServer.new
+    Signal.trap('INT') { stop }
+    Signal.trap('TERM') { stop }
+
+    log = Logging.logger['shadowsocks_log']
+    log.add_appenders Logging.appenders.file('/tmp/shadowsocks.log')
+    Yyrp.configure do |config|
+      config.logger = log
+      config.logger.level = :debug
+    end
+
+    server.start
+  }
+end
+
+def get_pid
+  if File.exists?(@pid_full)
+    file = File.new(@pid_full, 'r')
+    pid = file.read
+    file.close
+    pid
+  else
+    0
+  end
+end
+
+def start
+  pid = get_pid
+  if pid != 0
+    warn 'Daemon is already running'
+    exit -1
+  end
+
+  pid = fork {
+    run
+  }
+  begin
+    file = File.new(@pid_full, 'w')
+    file.write(pid)
+    file.close
+    Process.detach(pid)
+  rescue => exc
+    Process.kill('TERM', pid)
+    warn "Cannot start daemon: #{exc.message}"
+  end
+end
+
+def stop
+  pid = get_pid
+  begin
+    EM.stop
+  rescue => exc
+    warn "Stop server exception: #{exc.message}"
+  end
+
+  if pid != 0
+    Process.kill('HUP', pid.to_i)
+    File.delete(@pid_full)
+    warn 'Stopped'
+  else
+    warn 'Daemon is not running'
+    exit -1
+  end
+end
+
+# stop with `kill pid`
+start
